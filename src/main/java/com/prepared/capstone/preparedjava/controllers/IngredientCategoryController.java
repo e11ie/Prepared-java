73a1bf794833044a74bd7b6336a3a6d68b061226
E11ie <jessieadrian3@gmail.com>
2019-01-14 10:25:17 -0600

    added basic ingredient index, add and remove page & functionality

--- src/main/java/com/prepared/capstone/preparedjava/controllers/IngredientCategoryController.java
+++ src/main/java/com/prepared/capstone/preparedjava/controllers/IngredientCategoryController.java
@@ -1,13 +1,20 @@
 package com.prepared.capstone.preparedjava.controllers;
 
+import com.prepared.capstone.preparedjava.models.IngredientCategory;
 import com.prepared.capstone.preparedjava.models.data.IngredientCategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping(value="category")
+@RequestMapping(value="ingredient/category")
 public class IngredientCategoryController {
 
     @Autowired
@@ -20,5 +27,40 @@ public class IngredientCategoryController {
         model.addAttribute("title", "Ingredient Categories");
 
         return "ingredient-category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute("category", new IngredientCategory());
+        return "ingredient-category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add (Model model,
+                                @ModelAttribute @Valid IngredientCategory newCategory, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "ingredient-category/add";
+        }
+
+        categoryDao.save(newCategory);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String remove(Model model) {
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", "Remove Category");
+        return "ingredient-category/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String remove(@RequestParam int categoryId) {
+
+        categoryDao.delete(categoryId);
+
+        return "redirect:";
     }
 }
--- src/main/java/com/prepared/capstone/preparedjava/controllers/IngredientController.java
+++ src/main/java/com/prepared/capstone/preparedjava/controllers/IngredientController.java
@@ -1,4 +1,67 @@
 package com.prepared.capstone.preparedjava.controllers;
 
+
+import com.prepared.capstone.preparedjava.models.Ingredient;
+import com.prepared.capstone.preparedjava.models.data.IngredientDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value="ingredient")
 public class IngredientController {
+
+    @Autowired
+    private IngredientDao ingredientDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("ingredients", ingredientDao.findAll());
+        model.addAttribute("title", "Ingredients");
+
+        return "ingredients/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute("ingredient", new Ingredient());
+        return "ingredients/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add (Model model,
+                       @ModelAttribute @Valid Ingredient newIngredient, Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Ingredient");
+            return "ingredients/add";
+        }
+
+        ingredientDao.save(newIngredient);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String remove(Model model) {
+        model.addAttribute("ingredients", ingredientDao.findAll());
+        model.addAttribute("title", "Remove Ingredient");
+        return "ingredients/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String remove(@RequestParam int ingredientId) {
+
+        ingredientDao.delete(ingredientId);
+
+        return "redirect:";
+    }
 }
