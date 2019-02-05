1b8e108bd4c19cf41f6f2815e59b8809b57d1cfa
E11ie <jessieadrian3@gmail.com>
2019-02-05 10:39:50 -0600

    changed recipe index page added recipe/view/{id} : view individual recipes added recipe/add : add recipe (just 1 field, recipe title)

--- src/main/java/com/prepared/capstone/preparedjava/controllers/RecipeController.java
+++ src/main/java/com/prepared/capstone/preparedjava/controllers/RecipeController.java
@@ -1,12 +1,20 @@
 package com.prepared.capstone.preparedjava.controllers;
 
+import com.prepared.capstone.preparedjava.models.Recipe;
 import com.prepared.capstone.preparedjava.models.data.RecipeDao;
 import com.prepared.capstone.preparedjava.models.data.RecipeIngredientDao;
 import com.prepared.capstone.preparedjava.models.data.RecipeNoteDao;
+import com.prepared.capstone.preparedjava.models.forms.AddRecipeIngredientForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value="recipes")
@@ -30,10 +38,46 @@ public class RecipeController {
         return "recipes/index";
     }
 
-    // TODO - recipes/add
+    // View one Recipe
+    @RequestMapping(value = "view/{id}", method = RequestMethod.GET)
+    public String viewRecipe(Model model, @PathVariable("id") int id) {
+
+        Recipe recipe =  recipeDao.findOne(id);
+
+        model.addAttribute("title", "View Recipe: " + recipe.getTitle());
+//        model.addAttribute("cheeses", menu.getCheeses());
+//        model.addAttribute("menuId", menu.getId());
+
+        return "recipes/view";
+    }
+
+    // Add Recipe
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayRecipeForm(Model model) {
+        model.addAttribute("title", "Add Recipe");
+        model.addAttribute(new Recipe());
+
+        return "recipes/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+                                     Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Recipe");
+            return "recipes/add";
+        }
+
+        recipeDao.save(newRecipe);
+
+        return "redirect:view/" + newRecipe.getId();
+    }
+
+
+
     // TODO - recipes/delete
-    // TODO - recipes/{id} - view individual recipe
-    // TODO - recipes/edit/{id}
+    // TODO - recipes/edit/{id} - edit Title
     // TODO - recipes/edit/{id}/add-note
     // TODO - recipes/edit/{id}/add-ingredient
     // TODO - recipes/edit/{id}/delete-note
---
+++ src/main/java/com/prepared/capstone/preparedjava/models/forms/AddRecipeIngredientForm.java
@@ -0,0 +1,17 @@
+package com.prepared.capstone.preparedjava.models.forms;
+
+import com.prepared.capstone.preparedjava.models.Ingredient;
+import com.prepared.capstone.preparedjava.models.Unit;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class AddRecipeIngredientForm {
+
+    private Iterable<Unit> units;
+    private Iterable<Ingredient> ingredients;
+
+    //need MenuId (Menu) & amount & unitId (unit) & ingredientId (ingredient)
+
+
+}
